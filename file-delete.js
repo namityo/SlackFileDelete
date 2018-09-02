6d04dbcfc18c7f2cbe3dfa30dcb01e189502da7c
Namiki Kazuma <namityo@hotmail.com>
2018-09-02 18:06:26 +0900

    検索期間をパラメータで設定できるように修正

--- file-delete.js
+++ file-delete.js
@@ -14,21 +14,26 @@ var excludedExtensionsUpperCase = require('./config.json').excludedExtensions.ma
 
 // 検索パラメータ
 var param = {
-    ts_from : moment().subtract(3, 'hour').unix(), // 3時間前までのファイルを取得
+    ts_from : moment().subtract(
+        require('./config.json').param.from.value,
+        require('./config.json').param.from.type
+    ).unix(),
 }
 
 // ファイルを検索
 const web = new WebClient(token);
 slackFileUtil.getFileList(web, param, files => {
     // ヒットしたファイルを出力
+    console.log("------ Files uploaded after [" + moment.unix(param.ts_from).format() + "] ------")
     files.forEach(c => console.log(c.name + ' ' + c.id + ' ' + moment.unix(c.created).format()));
 
     // 削除除外する拡張子をチェック
+    console.log("------ Delete files ------")
     files.forEach(f => {
         var result = excludedExtensionsUpperCase.indexOf(f.filetype.toUpperCase());
         if (result == -1) {
             // 除外拡張子に見つからない場合は削除対象のため削除
-            console.log('delete : ' + f.name + ' [' + f.id + ']');
+            console.log(f.name + ' [' + f.id + ']');
             slackFileUtil.deleteFile(web, f.id);
         }
     });
